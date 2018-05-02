e64da96aef3c15d4bdbdded3e56aa8cfbc74881e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-02 19:48:38 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection_array)
   i = 0
   while i < collection_array.length
     collection_array.each do |language|
-      language.upcase
+      collection << language.upcase
     end
     i += 1
   end
