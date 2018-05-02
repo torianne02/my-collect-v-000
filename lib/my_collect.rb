a0ccd1ac2f841b80dc5c8f07049a166acf2f8b56
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-02 20:01:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,9 +4,9 @@ def my_collect(collection)
   while i < collection.length
     collection.each do |one_part|
       if one_part.length <= 1
-        yield new_collection << one_part.upcase
+        new_collection << one_part.upcase
       elsif one_part.length > 1
-        yield new_collection << one_part.split(" ").first
+        new_collection << one_part.split(" ").first
       end
     end
     i += 1
