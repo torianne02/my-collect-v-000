22d16cfb557cd8818441aa3675b5921c71a36fe9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-02 19:57:55 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,20 +3,11 @@ def my_collect(languages)
   collection = []
   while i < languages.length
     languages.each do |language|
-      collection << language.upcase
-    end
-    i += 1
-    return collection
-  end
-end
-
-def my_collect(students)
-  i = 0
-  collection = []
-  while i < students.length
-    students.each do |student|
-      collection << student.split(" ").first
-    end
+      if language.length == 1
+        collection << language.upcase
+      elsif language.length > 1
+        collection << student.split(" ").first
+      end
     i += 1
     return collection
   end
