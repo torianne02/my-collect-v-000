db2870f52bdf7e0561e44d0ec19a3b2976905080
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-02 19:41:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(collection)
   i = 0
-  while i < array.length
-    collection << yield(array[i])
+  while i < collection.length
+    collection << yield(collection[i])
     i += 1
   end
 end
